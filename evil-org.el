88a44ba1b49970a38c2fd5b32a7e2266b523c7f2
Ben Moon <guiltydolphin@gmail.com>
2016-08-21 20:25:25 +0100

    Add main .el file

---
+++ evil-org.el
@@ -0,0 +1,23 @@
+;;; evil-org.el --- Evil extensions for Org.
+;;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;;
+;;; Provide utilities for generating hooks.
+;;;
+;;; Code:
+
+(provide 'evil-org)
+;;; evil-org.el ends here
