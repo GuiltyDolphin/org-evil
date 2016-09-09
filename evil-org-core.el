4cc92ba1404ed8e29d67af7eef14927eb68ba783
Ben Moon <guiltydolphin@gmail.com>
2016-09-09 18:41:47 +0100

    Add macro for defining regional minor modes

--- evil-org-core.el
+++ evil-org-core.el
@@ -22,10 +22,31 @@
 ;;;
 ;;; Code:
 
+(require 'hook)
+
 (defgroup evil-org nil
   "Evil extensions for Org."
   :group 'evil
   :prefix 'evil-org-)
+
+(defmacro evil-org--define-regional-minor-mode (mode doc pred &rest args)
+  "Define an evil-org minor mode MODE that is active when PRED is non-NIL.
+DOC is the documentation as in `define-minor-mode'.
+
+PRED is checked after moving `point', and should be an un-quoted expression.
+ARGS should be the same as in `define-minor-mode' (bar MODE and DOC)."
+  (declare (doc-string 2)
+           (debug (&define name string-or-null-p sexp
+			   [&rest [keywordp sexp]]
+			   def-body)))
+  (let ((check-fn (intern (format "evil-org--check-%s" mode))))
+    `(progn
+       (define-minor-mode ,mode ,doc ,@args)
+       (defun ,check-fn ()
+         ,(format "Check whether %s should be activated in the current location." mode)
+         (if ,pred (,mode) (when ,mode (,mode -1))))
+       (hook--monitor-expression-value '(point) ',check-fn 'org-mode t))))
+(put 'evil-org--define-regional-minor-mode 'lisp-indent-function 'defun)
 
 (provide 'evil-org-core)
 ;;; evil-org-core.el ends here
