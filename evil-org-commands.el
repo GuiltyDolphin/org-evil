9c7dff099db25de0d89c2e9017f4bc2ae54495cd
Ben Moon <guiltydolphin@gmail.com>
2016-09-09 12:25:28 +0100

    Add file for general commands

---
+++ evil-org-commands.el
@@ -0,0 +1,26 @@
+;;; evil-org-commands.el --- evil-org general commands.
+
+;; Copyright (C) 2016 Ben Moon
+;; Author: Ben Moon <guiltydolphin@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;;
+;;; Defines evil-org general commands.
+;;;
+;;; Code:
+
+(provide 'evil-org-commands)
+;;; evil-org-commands.el ends here
