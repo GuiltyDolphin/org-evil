d0fa340542c790b292b6e049ab0402ea3792a933
Ben Moon <guiltydolphin@gmail.com>
2016-09-10 11:48:08 +0100

    Update hook -> monitor dependency

--- org-evil-core.el
+++ org-evil-core.el
@@ -22,7 +22,7 @@
 ;;;
 ;;; Code:
 
-(require 'hook)
+(require 'monitor)
 
 (defgroup org-evil nil
   "Evil extensions for Org."
@@ -45,7 +45,7 @@ ARGS should be the same as in `define-minor-mode' (bar MODE and DOC)."
        (defun ,check-fn ()
          ,(format "Check whether %s should be activated in the current location." mode)
          (if ,pred (,mode) (when ,mode (,mode -1))))
-       (hook--monitor-expression-value '(point) ',check-fn 'org-mode t))))
+       (monitor--monitor-expression-value '(point) ',check-fn 'org-mode t))))
 (put 'org-evil--define-regional-minor-mode 'lisp-indent-function 'defun)
 
 (provide 'org-evil-core)
