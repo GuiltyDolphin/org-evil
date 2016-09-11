5c00f69caa7f077c28168190eb4d2bbb62a5c849
Ben Moon <guiltydolphin@gmail.com>
2016-09-11 13:02:35 +0100

    Improve commentary

--- org-evil.el
+++ org-evil.el
@@ -23,9 +23,20 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;;
-;;; Evil extensions for Org.
-;;;
+
+;; Org-evil provides integration between Evil and Org through
+;; various means such as contextual keybindings.
+;;
+;; Org-evil has a standard set of keybindings, with additional
+;; bindings that vary depending on the location and type of
+;; Org file being edited, for example some bindings are only
+;; active within a table.
+;;
+;; All the bindings aim to be fairly intuitive for Evil users,
+;; so there shouldn't be any huge surprises.
+;;
+;; For more information see the README.
+
 ;;; Code:
 
 (require 'org-evil-commands)
